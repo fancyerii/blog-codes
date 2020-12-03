045b039b37549d7eb466fb7d3591278c683f3872
lili19 <lili19@vipkid.com.cn>
2020-12-03 20:22:19 +0800

    format codes

--- webdriver-wechat/src/main/java/com/github/fancyerii/wechatdriver/DebugXPath.java
+++ webdriver-wechat/src/main/java/com/github/fancyerii/wechatdriver/DebugXPath.java
@@ -14,53 +14,53 @@ import java.util.List;
 public class DebugXPath {
     public static void main(String[] args) throws IOException {
         ChromeOptions options = new ChromeOptions();
-        WebDriver driver=new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         System.setProperty("webdriver.chrome.driver", "/home/lili/soft/chromedriver");
         debug(driver);
     }
 
 
     private static void debug(WebDriver driver) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in,"UTF-8"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
         String line;
-        while((line=br.readLine())!=null){
-            if(line.equals("quit")) break;
-            if(line.startsWith("eval:")){
-                String xpath=line.substring("eval:".length());
-                System.out.println("eval xpath: "+xpath);
+        while ((line = br.readLine()) != null) {
+            if (line.equals("quit")) break;
+            if (line.startsWith("eval:")) {
+                String xpath = line.substring("eval:".length());
+                System.out.println("eval xpath: " + xpath);
                 try {
                     List<WebElement> elems = driver.findElements(By.xpath(xpath));
-                    System.out.println("found: "+elems.size());
-                    for(WebElement elem:elems){
-                        String outerHtml=elem.getAttribute("outerHTML");
-                        if(outerHtml.length()>10000){
-                            outerHtml=outerHtml.substring(0, 10000)+"...";
+                    System.out.println("found: " + elems.size());
+                    for (WebElement elem : elems) {
+                        String outerHtml = elem.getAttribute("outerHTML");
+                        if (outerHtml.length() > 10000) {
+                            outerHtml = outerHtml.substring(0, 10000) + "...";
                         }
-                        System.out.println("****"+elem.getTagName()+", "+elem.getText()+", "+outerHtml);
+                        System.out.println("****" + elem.getTagName() + ", " + elem.getText() + ", " + outerHtml);
                     }
-                }catch(Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }else if(line.startsWith("click:")){
-                String xpath=line.substring("click:".length());
-                System.out.println("click xpath: "+xpath);
-                try{
-                    WebElement elem=driver.findElement(By.xpath(xpath));
+            } else if (line.startsWith("click:")) {
+                String xpath = line.substring("click:".length());
+                System.out.println("click xpath: " + xpath);
+                try {
+                    WebElement elem = driver.findElement(By.xpath(xpath));
                     elem.click();
-                }catch(Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }else if(line.startsWith("sendtxt:")){
-                String s=line.substring("sendtxt:".length());
-                String[] arr=s.split("###");
-                System.out.println("xpath: "+arr[0]+", send text: "+arr[1]);
-                try{
-                    WebElement elem=driver.findElement(By.xpath(arr[0]));
+            } else if (line.startsWith("sendtxt:")) {
+                String s = line.substring("sendtxt:".length());
+                String[] arr = s.split("###");
+                System.out.println("xpath: " + arr[0] + ", send text: " + arr[1]);
+                try {
+                    WebElement elem = driver.findElement(By.xpath(arr[0]));
                     elem.sendKeys(arr[1]);
-                }catch(Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }else{
+            } else {
                 System.out.println("未知命令!!! 目前仅支持：\n1. eval:XPATH-->查看xpath\n2. click:XPATH-->点击XPATH的元素\n" +
                         "3.sendtxt:XPATH###TEXT-->给XPATH的元素发送文本TEXT\n如果想退出请输入quit");
             }
